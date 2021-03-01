ee5b5fc59b469ea46bca056eda6a42e4b4f16674
Adiluhung Setya P <adiluhung410@gmail.com>
2021-03-01 11:10:13 +0700

    UPDATE: add handler for customer

--- controller/UserHandler.go
+++ controller/UserHandler.go
@@ -1,1 +1,76 @@
 package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+	"user-go-test/model"
+	"user-go-test/service"
+	"user-go-test/utils"
+)
+
+type UserHandler struct {
+	userService service.UserService
+}
+
+func CreateUserHandler(r *gin.Engine, userService service.UserService)  {
+	userHandler := UserHandler{userService: userService}
+	r.GET("/user", userHandler.getAllUser)
+}
+
+func (u UserHandler) getAllUser(c *gin.Context) {
+	user, err := u.userService.GetAllUser()
+	if err != nil {
+		utils.HandleError(c, http.StatusBadGateway, err.Error())
+	}
+	utils.HandleSuccess(c, user)
+}
+
+func (u UserHandler) insertNewUser(c *gin.Context)  {
+	var body model.User
+
+	err := c.Bind(&body)
+	if err != nil {
+		utils.HandleError(c, http.StatusBadGateway, err.Error())
+	}
+	if body.Name == "" || body.Ktp == "" {
+		utils.HandleError(c, http.StatusBadRequest, "There's empty field")
+	}
+
+	user, err := u.userService.InsertNewUser(&body)
+	if err != nil {
+		utils.HandleError(c, http.StatusInternalServerError, "Server error")
+	}
+	utils.HandleSuccess(c, user)
+}
+
+func (u UserHandler) updateUser(c *gin.Context)  {
+	var body model.User
+	err := c.Bind(&body)
+	if err != nil {
+		utils.HandleError(c, http.StatusInternalServerError, err.Error())
+	}
+	body.Id, err= strconv.Atoi(c.Param("id"))
+	if err != nil {
+		utils.HandleError(c, http.StatusBadRequest, "Param must integer")
+	}
+	if body.Name == "" || body.Ktp == "" {
+		utils.HandleError(c, http.StatusBadRequest, "There's empty field")
+	}
+
+	user, err := u.userService.UpdateUser(body.Id,&body)
+	if err != nil {
+		utils.HandleError(c, http.StatusInternalServerError, "Server error")
+	}
+	utils.HandleSuccess(c, user)
+}
+
+func (u UserHandler) getUserById(c *gin.Context){
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		utils.HandleError(c, http.StatusBadRequest, "Id must be integer")
+	}
+	user,err := u.userService.GetUser(id)
+
+	utils.HandleSuccess(c, user)
+}
--- utils/Response.go
+++ utils/Response.go
@@ -1,7 +1,25 @@
 package utils
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+	"user-go-test/model"
+)
 
 func HandleSuccess(c *gin.Context, data interface{})  {
+	responseData := model.Response{
+		Status:  "200",
+		Message: "Success",
+		Data:    data,
+	}
+	c.JSON(http.StatusOK,responseData)
+}
 
+func HandleError(c *gin.Context, status int, message string)  {
+	responseData := model.Response{
+		Status:  strconv.Itoa(status),
+		Message: message,
+	}
+	c.JSON(status,responseData)
 }
